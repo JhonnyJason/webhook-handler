2b88da70a18dd2ec7a641638c47cfab20e5b74c8
JhonnyJason <lenny09918050@gmail.com>
2019-09-04 14:58:30 +0000

    implemented to be oneshot capable systemd service which terminates after first received request

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "mode": "production",
+    "devtool": "none",
+    "target": "node",
+    "entry": "/home/lenny/gits/webhook-handler/toolset/compiled/js/index.js",
+    "output": {
+        "filename": "service.js",
+        "path": "/home/lenny/gits/webhook-handler/output"
+    }
+}
